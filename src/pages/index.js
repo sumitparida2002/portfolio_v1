7989207a04195b23529033d0d83e121fcf78456f
Sumit Parida <80693203+sumitparida2002@users.noreply.github.com>
2023-09-25 11:13:51 -0400

    Form Email

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,13 +65,13 @@ const IndexPage = () => {
               </div>
               <ul className="flex flex-col items-center justify-between min-h-[250px]">
                 <li className="border-b border-gray-400 my-8 uppercase">
-                  <a href="/about">About</a>
+                  <a href="#about">About</a>
                 </li>
                 <li className="border-b border-gray-400 my-8 uppercase">
-                  <a href="/portfolio">Portfolio</a>
+                  <a href="#projects">Projects</a>
                 </li>
                 <li className="border-b border-gray-400 my-8 uppercase">
-                  <a href="/contact">Contact</a>
+                  <a href="#contact">Contact</a>
                 </li>
               </ul>
             </div>
@@ -110,8 +110,8 @@ const IndexPage = () => {
       `}</style>
       </div>
 
-      <div className="flex px-10">
-        <aside className="flex flex-col gap-6">
+      <div className="flex p-1   md:px-10">
+        <aside className="flex invisible lg:visible flex-col gap-6 ">
           <a target="_blank" href="https://github.com/sumitparida2002">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -155,7 +155,7 @@ const IndexPage = () => {
         <main className="flex">
           <div className="content lg:w-[80%] mx-auto">
             <section
-              className="lg:w-[70%] mt-[4rem] mx-auto md:mt-[6rem] pb-[8rem] sec"
+              className="lg:w-[70%] mt-[4rem]  mx-auto md:mt-[6rem] pb-[8rem] sec"
               id="landing"
             >
               <p className="text-[#64cc95] dark:text-[#64CCC5] ">
@@ -488,46 +488,89 @@ const IndexPage = () => {
               </p>
 
               <div className="w-[100%]">
-                <div className="flex justify-between mt-16 w-[100%]">
-                  <div className="w-[100%] flex justify-start flex-col ">
-                    <p className="dark:text-slate-300 text-slate-700 text-left ">
-                      Your Name
-                    </p>
-                    <input
-                      className="dark:bg-[#001C30] border-b border-[#64CCC5] focus:border-b-2 focus:outline-none  mt-1 w-[90%] text-left    "
-                      placeholder="Enter your name"
-                    ></input>
-                  </div>
+                <form method="post" action="https://formspree.io/f/xeqbrwar">
+                  <div className="flex md:flex-row flex-col justify-between mt-16 w-[100%]">
+                    <div className="w-[100%] flex justify-start flex-col ">
+                      <p className="dark:text-slate-300 text-slate-700 text-left ">
+                        Your Name
+                      </p>
+                      <input
+                        className="dark:bg-[#001C30] border-b border-[#64CCC5] focus:border-b-2 focus:outline-none  mt-1 w-[100%] md:w-[90%] text-left    "
+                        placeholder="Enter your name"
+                        name="name"
+                      ></input>
+                    </div>
 
-                  <div className="w-[100%] flex justify-start flex-col">
-                    <p className="dark:text-slate-300 text-slate-700 text-left">
-                      Your Email
-                    </p>
-                    <input
-                      className="dark:bg-[#001C30] border-b border-[#64cc95] dark:border-[#64CCC5] focus:border-b-2 focus:outline-none mt-1 w-[100%] "
-                      placeholder="Enter your email"
-                    ></input>
+                    <div className="w-[100%] mt-8 md:mt-0 flex justify-start flex-col">
+                      <p className="dark:text-slate-300 text-slate-700 text-left">
+                        Your Email
+                      </p>
+                      <input
+                        className="dark:bg-[#001C30] border-b border-[#64cc95] dark:border-[#64CCC5] focus:border-b-2 focus:outline-none mt-1 w-[100%] "
+                        placeholder="Enter your email"
+                        name="email"
+                      ></input>
+                    </div>
                   </div>
-                </div>
-                <div className="mt-8">
-                  <p className="dark:text-slate-300 text-slate-700 text-left">
-                    Your Message
-                  </p>
-                  <textarea
-                    className="dark:bg-[#001C30] border-b border-[#64cc95] dark:border-[#64CCC5] mt-1 h-auto w-[100%] message focus:outline-none  focus:border-b-2 "
-                    placeholder="Enter your name"
-                  ></textarea>
-                </div>
-                <button className=" mt-8 rounded py-2 px-8 text-[#64cc95] dark:text-[#64CCC5] border-2 border-[#64cc95] dark:border-[#64CCC5] hover:bg-[#64cc95] hover:text-white hover:border-[#64cc95] dark:hover:bg-[#64CCC5] dark:hover:text-white dark:hover:border-[#64CCC5] ">
-                  Send
-                </button>
+                  <div className="mt-8">
+                    <p className="dark:text-slate-300 text-slate-700 text-left">
+                      Your Message
+                    </p>
+                    <textarea
+                      className="dark:bg-[#001C30] border-b border-[#64cc95] dark:border-[#64CCC5] mt-1 h-auto w-[100%] message focus:outline-none  focus:border-b-2 "
+                      placeholder="Enter your message"
+                      name="message"
+                    ></textarea>
+                  </div>
+                  <button className=" mt-8 rounded py-2 px-8 text-[#64cc95] dark:text-[#64CCC5] border-2 border-[#64cc95] dark:border-[#64CCC5] hover:bg-[#64cc95] hover:text-white hover:border-[#64cc95] dark:hover:bg-[#64CCC5] dark:hover:text-white dark:hover:border-[#64CCC5] ">
+                    Send
+                  </button>
+                </form>
               </div>
             </section>
           </div>
         </main>
-        <aside className="sidebar2"></aside>
+        <aside className="sidebar2 invisible lg:visible"></aside>
       </div>
-      <footer className="mt-[6rem] px-10  scroll-child mb-[.5rem]">
+      <div className="flex lg:hidden mt-[6rem]  gap-6 justify-center">
+        <a target="_blank" href="https://github.com/sumitparida2002">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            class="bi bi-github text-slate-400 hover:text-[#64cc95] dark:hover:text-[#64CCC5]"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+          </svg>
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/sumitparida/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            class="bi bi-linkedin text-slate-400 hover:text-[#64cc95] dark:hover:text-[#64CCC5]"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+          </svg>
+        </a>
+        <a target="_blank" href="https://www.instagram.com/sumitparida/?hl=en">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            class="bi bi-instagram text-slate-400 hover:text-[#64cc95] dark:hover:text-[#64CCC5]"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
+          </svg>
+        </a>
+      </div>
+      <footer className="mt-4 px-10 lg:mt-[6rem]  scroll-child mb-[.5rem]">
         <p className="w-[100%] text-sm  text-center text-slate-300">
           Designed and Developed by Sumit Parida
         </p>
